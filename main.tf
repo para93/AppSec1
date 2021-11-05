c08751258442c2ba053f30ce35f761219ff0a84c
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 22:36:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+terraform {
+  required_providers {
+     aws = {
+      source = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
 provider "aws" {
-  region = "ap-south-1"
-  profile = "blackhawk"
+  region = "us-east-2"
 }
